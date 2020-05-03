47e44b5efa4c064a12614c3f185e9a40006273fc
Panchajanya Sarkar <panchajanya@azure-dev.live>
2020-05-03 09:31:25 +0000

    install: Minimal Installation
    Signed-off-by: Panchajanya Sarkar <panchajanya@azure-dev.live>

--- install_deps.sh
+++ install_deps.sh
@@ -7,15 +7,11 @@ sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 sudo pacman -Syyu --noconfirm
 
 # Install basic packages
-sudo pacman -S --noconfirm base-devel \
-                 git wget  \
+sudo pacman -S --noconfirm \
+                 wget  \
                  cmake svn lzip patchelf \
                  inetutils python2 lld llvm \
                  clang gcc gcc-libs g++ bc ccache multilib-devel glibc
 
 export PATH=/usr/bin/:$PATH
 export LD_LIBRARY_PATH=/usr/lib64/:$LD_LIBRARY_PATH
-
-yay -S --noconfirm ncurses5-compat-libs \
-                lib32-ncurses5-compat-libs \
-                aosp-devel xml2 lineageos-devel
