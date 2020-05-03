35258bf64138fcdfd16cc1ccf5dfd314e588f2ad
Panchajanya Sarkar <panchajanya@azure-dev.live>
2020-05-03 05:35:13 +0000

    install: Install bc and ccache
    Signed-off-by: Panchajanya Sarkar <panchajanya@azure-dev.live>

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,15 @@ ADD archlinux.tar /
 ENV LANG=en_US.UTF-8
 CMD ["/usr/bin/bash"]
 
+RUN pacman -Syuq --noconfirm git base-devel sudo
+
+#RUN echo "Defaults         lecture = never" > /etc/sudoers.d/privacy \
+ #&& echo "%wheel ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/wheel \
+ #&& useradd -m -G wheel -s /bin/bash builder
+
+USER builder
+RUN mkdir -p /home/builder
+
 # Install all required packages
 COPY install_deps.sh /tmp/
 RUN bash /tmp/install_deps.sh
--- install_deps.sh
+++ install_deps.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 # Uncomment the community [multilib] repository
-sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
 # Run pacman to update
-pacman -Syyu --noconfirm
+sudo pacman -Syyu --noconfirm
 
 # Install basic packages
-pacman -S --noconfirm base-devel \
+sudo pacman -S --noconfirm base-devel \
                  git wget multilib-devel \
                  cmake svn lzip patchelf \
-                 inetutils python3 lld llvm \
-                 clang gcc
+                 inetutils python2 lld llvm \
+                 clang gcc g++ bc ccache
+
+#export PATH=/usr/bin/gcc:$PATH
+cd /home/builder
 
 # Install ncurses5-compat-libs, lib32-ncurses5-compat-libs, aosp-devel, xml2, and lineageos-devel
 for package in ncurses5-compat-libs lib32-ncurses5-compat-libs aosp-devel xml2 lineageos-devel; do
         git clone https://aur.archlinux.org/"${package}"
-        cd "${package}" || continue
-        makepkg -si --skippgpcheck
+        cd ${package} || continue
+        makepkg -sif --noconfirm --skippgpcheck
+        #sudo pacman -U --noconfirm /home/builder/pkg/*.tar.xz
         cd - || break
         rm -rf "${package}"
     done
-
